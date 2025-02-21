483341c6b6dd6e705c9d2cc98ba511198ea29457
aitrends.now <aitrends.now@gmail.com>
2025-02-21 19:20:30 +0800

    sending awesome

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteCompression from 'vite-plugin-compression';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-    plugins: [
-        react(),
-        visualizer({ open: false }),
-        viteCompression(),
-        imagetools(),
-    ],
-    css: {
-        postcss: './postcss.config.cjs', // Only if you need PostCSS
-    },
-    build: {
-        minify: 'esbuild', // Default, but can be explicitly set
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    const fileName = assetInfo.name || 'default-asset-name';
-                    let extType = fileName.split('.').at(-1) || 'unknown';
+plugins: [
+react(),
+ViteMinifyPlugin(),
+visualizer({ open: false }),
+viteCompression(), // No headers needed
+imagetools(),
+],
+css: {
+postcss: './postcss.config.cjs',
+},
+build: {
+rollupOptions: {
+output: {
+assetFileNames: (assetInfo) => {
+const fileName = assetInfo.name || 'default-asset-name';
+let extType = fileName.split('.').at(-1) || 'unknown';
 
-                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                        extType = 'img';
-                    } else if (/woff|woff2/.test(extType)) {
-                        extType = 'fonts';
-                    }
-                    return `assets/${extType}/[name]-[hash][extname]`;
-                },
-                chunkFileNames: 'assets/js/[name]-[hash].js',
-                entryFileNames: 'assets/js/[name]-[hash].js',
-            },
-        },
-        outDir: 'dist',
-    },
-    base: '/aitrends.now/', // Adjust if deploying to the root
+if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+extType = 'img';
+} else if (/woff|woff2/.test(extType)) {
+extType = 'fonts';
+}
+return `assets/${extType}/[name]-[hash][extname]`;
+},
+chunkFileNames: 'assets/js/[name]-[hash].js',
+entryFileNames: 'assets/js/[name]-[hash].js',
+},
+},
+outDir: 'dist',
+},
+base: '/aitrends.now/', // Fixed base path
 });
