25afb20fdb59b11dfeb54aa32677f1932779a581
aitrends.now <aitrends.now@gmail.com>
2025-02-21 21:10:31 +0800

    sending awesome

--- vite.config.ts
+++ vite.config.ts
@@ -6,35 +6,37 @@ import viteCompression from 'vite-plugin-compression';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-plugins: [
-react(),
-ViteMinifyPlugin(),
-visualizer({ open: false }),
-viteCompression(), // No headers needed
-imagetools(),
-],
-css: {
-postcss: './postcss.config.cjs',
-},
-build: {
-rollupOptions: {
-output: {
-assetFileNames: (assetInfo) => {
-const fileName = assetInfo.name || 'default-asset-name';
-let extType = fileName.split('.').at(-1) || 'unknown';
+  plugins: [
+    react(),
+    ViteMinifyPlugin(),
+    visualizer({ open: false }),
+    viteCompression({ algorithm: 'brotliCompress' }), // Brotli compression enabled
+    imagetools(),
+  ],
+  css: {
+    postcss: './postcss.config.cjs',
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const fileName = assetInfo.name || 'default-asset-name';
+          let extType = fileName.split('.').pop() || 'unknown';
 
-if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-extType = 'img';
-} else if (/woff|woff2/.test(extType)) {
-extType = 'fonts';
-}
-return `assets/${extType}/[name]-[hash][extname]`;
-},
-chunkFileNames: 'assets/js/[name]-[hash].js',
-entryFileNames: 'assets/js/[name]-[hash].js',
-},
-},
-outDir: 'dist',
-},
-base: '/aitrends.now/', // Fixed base path
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/woff|woff2/.test(extType)) {
+            extType = 'fonts';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+    outDir: 'dist',
+    sourcemap: true, // Enables source maps for debugging
+    minify: 'terser', // Use Terser for better minification
+  },
+  base: '/aitrends.now/', // Fixed base path for GitHub Pages deployment
 });
