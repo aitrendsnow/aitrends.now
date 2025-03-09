9ceeeaf00b69d82efcd2f1b6a7307a3bee4d5e4e
aitrends.now <aitrends.now@gmail.com>
2025-03-09 22:07:35 +0800

    sending awesome

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// Import font files to ensure Vite processes them
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./bootstrap-overrides.css";
+
 import "./assets/fonts/GoogleSans-Regular.woff2";
 import "./assets/fonts/GoogleSans-Medium.woff2";
 import "./assets/fonts/GoogleSans-Bold.woff2";
