8b74f60b519e19eb6eda75aa3744e1133360f307
aitrends.now <aitrends.now@gmail.com>
2025-03-10 18:30:39 +0800

    sending awesome

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./bootstrap-overrides.css";
+// Preload and serve Bootstrap Icons fonts from src/assets/fonts/
+import "./assets/fonts/bootstrap-icons.woff2";
+import "./assets/fonts/bootstrap-icons.woff";
 
 import "./assets/fonts/GoogleSans-Regular.woff2";
 import "./assets/fonts/GoogleSans-Medium.woff2";
